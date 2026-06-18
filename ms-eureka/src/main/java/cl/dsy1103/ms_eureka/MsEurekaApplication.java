8580d568354461ec707fd9106db171a59414fd47
Nicky Valero <nic.valero@duocuc.cl>
2026-06-18 09:16:31 -0400

    Add files via upload

--- ms-eureka/src/main/java/cl/dsy1103/ms_eureka/MsEurekaApplication.java
+++ ms-eureka/src/main/java/cl/dsy1103/ms_eureka/MsEurekaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer // Anotación fundamental para activar el ciclo de vida de Eureka Server
+@EnableEurekaServer
 public class MsEurekaApplication {
 
 	public static void main(String[] args) {
