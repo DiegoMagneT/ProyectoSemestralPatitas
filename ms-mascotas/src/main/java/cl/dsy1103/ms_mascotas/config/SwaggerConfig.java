421c5a69d069c2cae7d98948984db3576ce13122
Nicky Valero <nic.valero@duocuc.cl>
2026-06-18 09:18:41 -0400

    Add files via upload

---
+++ ms-mascotas/src/main/java/cl/dsy1103/ms_mascotas/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package cl.dsy1103.ms_mascotas.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Mascotas")
+                        .description("Sistema Patitas - API Mascotas")
+                        .version("v1.0"));
+    }
+}
